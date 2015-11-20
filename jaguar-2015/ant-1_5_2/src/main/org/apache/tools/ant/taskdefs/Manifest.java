be1d5ed1e10ac53b762f479cdbe52f6dec6922ad
Higor Amario de Souza <higoramario@gmail.com>
2015-11-19 19:58:24 -0500

    ant 1.5.2. (v5), fault MST_AK_1

--- jaguar-2015/ant-1_5_2/src/main/org/apache/tools/ant/taskdefs/Manifest.java
+++ jaguar-2015/ant-1_5_2/src/main/org/apache/tools/ant/taskdefs/Manifest.java
@@ -740,7 +740,7 @@ public class Manifest {
 
 
 
-                    = new Manifest(new InputStreamReader(in, "UTF-8"));
+                    = new Manifest(new InputStreamReader(in, "UTF-16"));
 
 
 
