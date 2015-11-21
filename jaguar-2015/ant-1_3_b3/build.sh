bafe05bc0091303423eb5d984ced1b74757d1cb9
Higor Amario de Souza <higoramario@gmail.com>
2015-11-21 17:27:25 -0500

    ant 1.3 beta-3 (v1), fault CLJ_HD_1

---
+++ jaguar-2015/ant-1_3_b3/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+CLASSPATH="$CLASSPATH":.
+export CLASSPATH
+echo $CLASSPATH
+# Set install directory here
+REALANTHOME=/home/higor/Dropbox/experiments/apache-ant/ant_build
+#REALANTHOME=$ANT_HOME
+ANT_HOME=bootstrap
+export ANT_HOME
+
+if test ! -f bootstrap/lib/ant.jar -o  ! -x bootstrap/bin/ant -o ! -x bootstrap/bin/antRun ; then
+  /bin/sh ./bootstrap.sh
+fi    
+
+LOCALCLASSPATH=lib/parser.jar:lib/jaxp.jar:lib/optional/junit.jar
+# add in the dependency .jar files
+DIRLIBS=lib/optional/*.jar
+for i in ${DIRLIBS}
+do
+    if [ "$i" != "${DIRLIBS}" ] ; then
+        LOCALCLASSPATH=$LOCALCLASSPATH:"$i"
+    fi
+done
+
+CLASSPATH=$LOCALCLASSPATH:$CLASSPATH
+export CLASSPATH
+
+if [ "$REALANTHOME" != "" ] ; then
+  ANT_INSTALL="-Dant.install=$REALANTHOME"
+fi
+
+bootstrap/bin/ant $ANT_INSTALL $*
+
+javac AntTestSuite.java
